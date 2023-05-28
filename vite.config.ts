89630fceb33b84c5ac330218847b6825c30f3941
therealeddy <eddy.developerr@gmail.com>
2023-05-27 23:17:07 -0300

    fix: react-is

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react', 'react-dom', 'styled-components'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          'styled-components': 'styled',
-        },
-      },
-    },
-  },
   server: {
     port: 3000,
   },
