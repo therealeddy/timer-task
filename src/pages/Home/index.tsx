7338b27fce1617b3616c25514ca930ea29a8333f
therealeddy <eddy.developerr@gmail.com>
2023-05-28 14:35:54 -0300

    feat: typescript on form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -23,17 +23,18 @@ const newCycleFormValidationSchema = zod.object({
     .max(60, 'O intervalo precisa ser de no máximo 60 minutos.'),
 })
 
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 const Home: React.FC = () => {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm({
+  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
   })
 
-  function handleCreateNewCycle(data: any) {
+  function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
   }
 
