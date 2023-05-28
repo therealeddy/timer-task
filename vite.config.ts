81ab4b4abea912695ff88cea16f6fe67d497c327
therealeddy <eddy.developerr@gmail.com>
2023-05-27 22:40:55 -0300

    fix: react-is

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import commonjs from 'vite-plugin-commonjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), commonjs()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          [
+            'babel-plugin-styled-components',
+            {
+              displayName: true,
+            },
+          ],
+        ],
+      },
+    }),
+  ],
   server: {
     port: 3000,
   },
